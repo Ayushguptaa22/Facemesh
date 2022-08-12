0eaf128ea81bea87744cb1303d9816a70597b4a4
Ayushguptaa22 <Ayushgupta_19021785.cse@geu.ac.in>
2022-08-12 23:16:13 +0530

    landmarks drawing completed

--- facemesh/src/utilities.js
+++ facemesh/src/utilities.js
@@ -2643,11 +2643,38 @@ export const TRIANGULATION = [
   ];
 
 // drawing triangles
+const drawPath= (ctx,points, closePath)=>{
+    const region = new Path2D();
+    region.moveTo(points[0][0], points[0][1]);
+    for(let i=1;i<points.length;i++){
+        const point=points[i];
+        region.lineTo(point[0],point[1]);
+    }
+    if(closePath){
+        region.closePath();
+    }
+    ctx.strokeStyle="aqua";
+    ctx.stroke(region);
+}
+
 // drawing points
 export const drawMesh= (predictions, ctx)=>{
     if(predictions.length>0){
+
+        // drawing points
         predictions.forEach(prediction=>{
             const keypoints = prediction.scaledMesh;
+
+            // drawing triangles
+            for(let i=0;i<TRIANGULATION.length/3;i++){
+            const points=[
+                TRIANGULATION[i*3],
+                TRIANGULATION[i*3+1],
+                TRIANGULATION[i*3+2],
+            ].map((index) => keypoints[index]);
+            drawPath(ctx,points,true);
+            }
+
             for(let i=0;i<keypoints.length; i++){
                 const x=keypoints[i][0]
                 const y=keypoints[i][1];
