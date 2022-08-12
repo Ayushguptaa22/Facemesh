2ff877c4bef92c50f18d82e4064a494573d0302d
Ayushguptaa22 <Ayushgupta_19021785.cse@geu.ac.in>
2022-08-12 22:14:31 +0530

    Libraries installed and imported

--- facemesh/src/App.js
+++ facemesh/src/App.js
@@ -1,9 +1,13 @@
+import React, {useRef} from 'react';
 import './App.css';
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from 'react-webcam';
 
 function App() {
   return (
     <div className="App">
-        Facemesh
+        Facemesh App
     </div>
   );
 }
